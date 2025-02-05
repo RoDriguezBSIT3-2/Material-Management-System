b8f01432b5a32286979a5060190ef1111f9ece2d
RoDriguezBSIT3-2 <132137046+RoDriguezBSIT3-2@users.noreply.github.com>
2025-02-06 02:33:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import os
 from sqlalchemy import func
-from werkzeug.utils import secure_filename
 from flask import send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import requests
